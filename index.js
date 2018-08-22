a43d703542c358375027c8de29c43531695bbb28
Ashley Pereira <ashleypereira70@gmail.com>
2018-08-22 21:06:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(element) {
+	return kittens.push(element);
+}
+
+function destructivelyPrependKitten(element) {
+  return kittens.unshift(element);
+}
+
+function destructivelyRemoveLastKitten(element) {
+  return kittens.pop(element);
+}
+
+function destructivelyRemoveFirstKitten(element) {
+  return kittens.shift(element);
+}
+
+function appendKitten(element){
+  return kittens.concat(element);
+}
+
+function prependKitten(element){ 
+  return [element,...kittens];
+}
+
+function removeLastKitten(element){
+ return kittens.slice(2);
+  }
